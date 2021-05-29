e6993b911a3d9b42a2e1f4c8d4a867f1e9274050
sviatdev <sviatiq@gmail.com>
2021-05-29 16:24:29 +0300

    #2 - Adding DB, adding and filtering data

--- src/main/java/com/myproject/lecturessharing/GreetingsController.java
+++ src/main/java/com/myproject/lecturessharing/GreetingsController.java
@@ -1,12 +1,19 @@
 package com.myproject.lecturessharing;
+import com.myproject.lecturessharing.domain.Message;
+import com.myproject.lecturessharing.repos.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
 public class GreetingsController {
+    @Autowired
+    private MessageRepo messageRepo;
 
     @GetMapping("/greeting")
     public String greeting(
@@ -18,7 +25,30 @@ public class GreetingsController {
 
     @GetMapping
     public String mainPage(Map<String, Object> model){
-        model.put("some", "hello, LS's users");
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages", messages);
+        return "mainpage";
+    }
+
+    @PostMapping
+    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model){
+        Message message = new Message(text, tag);
+        messageRepo.save(message);
+
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages", messages);
+        return "mainpage";
+    }
+
+    @PostMapping("filter")
+    public String get(@RequestParam String filter, Map<String, Object> model){
+        Iterable<Message> messages;
+        if(filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        }else{
+            messages = messageRepo.findAll();
+        }
+        model.put("messages", messages);
         return "mainpage";
     }
 }
