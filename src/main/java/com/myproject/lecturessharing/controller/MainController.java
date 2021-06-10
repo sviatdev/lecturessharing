ad84b7d644594e538995cddde49d9ef23fbd4cb6
sviatdev <sviatiq@gmail.com>
2021-06-10 19:21:08 +0300

    #4 - Added authority and freemarker template

--- src/main/java/com/myproject/lecturessharing/controller/MainController.java
+++ src/main/java/com/myproject/lecturessharing/controller/MainController.java
@@ -1,8 +1,11 @@
 package com.myproject.lecturessharing.controller;
 import com.myproject.lecturessharing.domain.Message;
+import com.myproject.lecturessharing.domain.User;
 import com.myproject.lecturessharing.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,15 +24,24 @@ public class MainController {
     }
 
     @GetMapping("/mainpage")
-    public String mainPage(Map<String, Object> model){
+    public String mainPage(@RequestParam(required = false, defaultValue = "") String filter, Model model){
         Iterable<Message> messages = messageRepo.findAll();
-        model.put("messages", messages);
+        if(filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        }else{
+            messages = messageRepo.findAll();
+        }
+        model.addAttribute("messages", messages);
+        model.addAttribute("filter", filter);
         return "mainpage";
     }
 
     @PostMapping("/mainpage")
-    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model){
-        Message message = new Message(text, tag);
+    public String add(
+            @AuthenticationPrincipal User user,
+            @RequestParam String text,
+            @RequestParam String tag, Map<String, Object> model){
+        Message message = new Message(text, tag, user);
         messageRepo.save(message);
 
         Iterable<Message> messages = messageRepo.findAll();
@@ -37,15 +49,4 @@ public class MainController {
         return "mainpage";
     }
 
-    @PostMapping("filter")
-    public String get(@RequestParam String filter, Map<String, Object> model){
-        Iterable<Message> messages;
-        if(filter != null && !filter.isEmpty()) {
-            messages = messageRepo.findByTag(filter);
-        }else{
-            messages = messageRepo.findAll();
-        }
-        model.put("messages", messages);
-        return "mainpage";
-    }
 }
---
+++ src/main/java/com/myproject/lecturessharing/service/UserService.java
@@ -0,0 +1,22 @@
+package com.myproject.lecturessharing.service;
+
+import com.myproject.lecturessharing.repos.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+    private final UserRepo userRepo;
+
+    public UserService(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepo.findByUsername(username);
+    }
+}
